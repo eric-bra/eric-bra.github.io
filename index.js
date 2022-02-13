1071fc6264fc1780914462a586f11f40a3e9f8ba
Eric Brake <83550924+eric-bra@users.noreply.github.com>
2022-02-13 23:54:59 +0100

    minor changes

--- index.js
+++ index.js
@@ -249,7 +249,7 @@ let supportsOrientationChange = "onorientationchange" in window,
     orientationEvent = supportsOrientationChange ? "orientationchange" : "resize";
 
 window.addEventListener(orientationEvent, function() {
-  let delayInMilliseconds = 10;
+  let delayInMilliseconds = 1000;
   setTimeout(showLocation, delayInMilliseconds);
   showLocation();
 }, true);
